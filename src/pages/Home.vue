557e6390bc97fbe2cb429fe592dd5ff9ae0e61a1
bekzod <bekzod.k2407@gmail.com>
2022-01-23 17:23:19 +0500

    added github user api

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -9,6 +9,17 @@
         />
 
         <button @click="getRepos" class="btn btnPrimary">Search!</button>
+
+        <!-- repository github -->
+
+        <div class="repos__wrapper" v-if="repos">
+          <div class="repos__item" v-for="repo in repos" :key="repo.id">
+            <div class="repo__info">
+              <a target="_blank" :href="repo.html_url">{{ repo.name }}</a>
+              <span>{{ repo.stargazers_count }}⭐</span>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   </div>
@@ -16,11 +27,13 @@
 
 <script>
 import search from "../components/Search.vue";
+import axios from "axios";
 export default {
   components: { search },
   data() {
     return {
       search: "",
+      repos: null,
     };
   },
   computed: {
@@ -29,13 +42,22 @@ export default {
     },
   },
   methods: {
-      getRepos() {
-          console.log(`got ${this.search} repository`);
-      }
-  }
+    getRepos() {
+      axios
+        .get(`https://api.github.com/users/${this.search}/repos`)
+        .then((res) => {
+          this.repos = res.data;
+          console.log(res);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+      console.log(`got ${this.search} repository`);
+    },
+  },
 };
 </script>
-
+// https://api.github.com/users/bekzod-2407
 <style lang="scss" scoped>
 .container {
   display: flex;
