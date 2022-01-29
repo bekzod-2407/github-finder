c6490348d8d261ed5fbcfab1fdd26a21ce00f440
bekzod <bekzod.k2407@gmail.com>
2022-01-29 12:28:34 +0500

    last 2

--- src/App.vue
+++ src/App.vue
@@ -29,4 +29,43 @@ export default {
     color: red;
   }
 }
+label {
+  display: block;
+  font-size: 16px;
+  margin-bottom: 10px;
+  text-align: center;
+}
+input {
+  padding: 16px 26px;
+  margin-bottom: 30px;
+  border-radius: 20px;
+}
+textarea {
+  padding: 30px 27px;
+  border-radius: 14px;
+}
+input,
+textarea {
+  width: 100%;
+}
+
+//
+.btn {
+  display: inline-block;
+  padding: 0.8em 2em;
+  line-height: 1;
+  text-transform: uppercase;
+  cursor: pointer;
+  border-radius: 30px;
+  &.btnPrimary {
+    color: #ffffff;
+    border: 1px solid #494ce8;
+    background-color: #494ce8;
+    transition: all 0.6s ease;
+    &:hover {
+      background-color: #ffffff;
+      color: #494ce8;
+    }
+  }
+}
 </style>
